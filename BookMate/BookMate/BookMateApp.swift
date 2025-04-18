b9101be68ca77cd934069f69c3a3ab218e9d83af
Abdül Samed Kara <abdulsamed_kara21@erdogan.edu.tr>
2025-04-18 02:09:26 -0700

    xcode
    fix

---
+++ BookMate/BookMate/BookMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMateApp.swift
+//  BookMate
+//
+//  Created by abdulsamed on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookMate/BookMateTests/BookMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookMateTests.swift
+//  BookMateTests
+//
+//  Created by abdulsamed on 18.04.2025.
+//
+
+import Testing
+@testable import BookMate
+
+struct BookMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
